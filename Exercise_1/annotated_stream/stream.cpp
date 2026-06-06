20a5b3d3aed183a170a50d0c0d3a185a64218b28
daphnefelt <daphnefelt@gmail.com>
2026-06-05 19:21:57 -0600

    got the frame annotated

--- Exercise_1/annotated_stream/stream.cpp
+++ Exercise_1/annotated_stream/stream.cpp
@@ -6,6 +6,7 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
@@ -14,30 +15,56 @@ using namespace std;
 #define ESCAPE_KEY (27)
 #define SYSTEM_ERROR (-1)
 
+// From exercise 1 for the image output
+#define OUTPUT_WIDTH (320)
+#define OUTPUT_HEIGHT (240)
+
 int main()
 {
    VideoCapture cam0(0);
    namedWindow("video_display");
    char winInput;
+   int saved_frames = 0;
 
    if (!cam0.isOpened())
    {
        exit(SYSTEM_ERROR);
    }
 
-   cam0.set(CAP_PROP_FRAME_WIDTH, 640);
-   cam0.set(CAP_PROP_FRAME_HEIGHT, 480);
+   // setting cam res to the output resolution I want
+   cam0.set(CAP_PROP_FRAME_WIDTH, OUTPUT_WIDTH);
+   cam0.set(CAP_PROP_FRAME_HEIGHT, OUTPUT_HEIGHT);
 
    while (1)
    {
       Mat frame;
       cam0.read(frame);
+
+      // Annotations
+
+      // Crosshair in middle
+      int cx = frame.cols / 2; // forcing these to int will round down
+      int cy = frame.rows / 2;
+      line(frame, Point(cx, 0), Point(cx, frame.rows), Scalar(0, 255, 255), 1);
+      line(frame, Point(0, cy), Point(frame.cols, cy), Scalar(0, 255, 255), 1);
+
+      // Border of 4 pixels
+      rectangle(frame, Point(0, 0), Point(frame.cols, frame.rows), Scalar(0, 0, 255), 4); // Doesn't specify color, I think red would look good
+
       imshow("video_display", frame);
 
       if ((winInput = waitKey(10)) == ESCAPE_KEY)
       //if ((winInput = waitKey(0)) == ESCAPE_KEY)
       {
           break;
+      }
+      else if(winInput == 's') // hotkey to save a frame
+      {
+        ostringstream filename;
+        filename << "frame_" << saved_frames << ".jpg";
+        imwrite(filename.str(), frame);
+        cout << "saved " << filename.str() << endl;
+        saved_frames++; // don't overwrite for next frame
       }
       else if(winInput == 'n')
       {
